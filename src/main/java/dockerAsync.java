d68321ae9d88ef31a72a6b49e4adde5d5d28cda9
isaacmg <igodfried@isaac26.com>
2018-12-02 21:15:45 -0500

    replace with basic

--- src/main/java/dockerAsync.java
+++ src/main/java/dockerAsync.java
@@ -1,9 +1,9 @@
 import org.apache.flink.streaming.api.functions.async.ResultFuture;
 import org.apache.flink.streaming.api.functions.async.RichAsyncFunction;
 
-public class dockerAsync extends RichAsyncFunction<BasicTweet, String > {
+public class dockerAsync extends RichAsyncFunction<TweetData, String > {
     @Override
-    public void asyncInvoke(BasicTweet basicTweet, ResultFuture<String> resultFuture) {
-        
+    public void asyncInvoke(TweetData tweet, ResultFuture<String> resultFuture) {
+        String s = tweet.tweetText;
     }
 }
