57155a39a426097152e7c0896b81eb50f31303a2
Isaac Godfried <igodfried@isaac26.com>
2019-02-27 02:18:28 -0500

    basic onnx worksing now

--- src/main/java/flairMap.java
+++ src/main/java/flairMap.java
@@ -13,14 +13,15 @@ public class flairMap extends RichMapFunction<TweetData, String> {
             config.addSharedModules("gensim");
             config.addSharedModules("torch");
             config.addSharedModules("sklearn");
-            //onfig.addSharedModules("flair");
+            config.addSharedModules("flair");
             SharedInterpreter theJep = new jep.SharedInterpreter();
             theJep.eval("from flair.data import Sentence");
             theJep.eval("from flair.models import SequenceTagger");
-            theJep.set("text", tweet.tweetText);
+            // Need to do more preprocessing on tweet.tweetText
+            theJep.set("text", "Some basic shit for the thing");
             theJep.eval("s=Sentence(text)");
             theJep.eval("model = SequenceTagger.load('ner')");
-            Object result = theJep.getValue("tagger.predict(s)");
+            Object result = theJep.getValue("model.predict(s)");
             System.out.println(result.toString());
             theJep.close();
             return result.toString();
@@ -30,10 +31,12 @@ public class flairMap extends RichMapFunction<TweetData, String> {
         }
         catch(jep.JepException e){
             e.printStackTrace();
+            throw e;
+            //theJep.close();
 
         }
 
-        return "";
+        //return "";
     }
 
 
