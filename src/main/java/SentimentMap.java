ee52c829f4f9b9840bf9712b673a5e2e27f62e80
Isaac Godfried <igodfried@isaac26.com>
2019-04-12 18:20:04 -0400

    update Sentiment

--- src/main/java/SentimentMap.java
+++ src/main/java/SentimentMap.java
@@ -4,9 +4,11 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public class SentimentMap implements RichMapFunction {
+public class SentimentMap implements RichMapFunction<String, List<>> {
     private Jep j;
     public void open(Configuration c)
     {
@@ -20,11 +22,20 @@ public class SentimentMap implements RichMapFunction {
             e.printStackTrace();
         }
     }
-    public Iterator<Tuple3<String, String, String>> map(TweetData tweet){
+    public List<Tuple3<String, String, String>> map(TweetData tweet){
         try {
             j.set("text", tweet.tweetText);
             j.eval("s=Sentence(text)");
             j.eval("result = classifier.predict(s)");
+            Object o = j.getValue("result");
+            System.out.println(o.toString());
+            ArrayList<Tuple3<String, String, String>> s = new ArrayList<>();
+            for (tup: tweet.entsLabels){
+                Tuple3<String, String, String> fullTup = new Tuple3<>();
+                
+
+            }
+            return s ;
 
         }
         catch (jep.JepException e){
