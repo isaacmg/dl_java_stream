3e0b55e388d1b5a84250203477af540cede4a66c
Isaac Godfried <igodfried@isaac26.com>
2019-04-30 01:16:37 -0400

    basic asyncio working

--- src/main/java/dockerAsync.java
+++ src/main/java/dockerAsync.java
@@ -1,14 +1,22 @@
 
+import org.apache.commons.io.IOUtils;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.functions.async.ResultFuture;
 import org.apache.flink.streaming.api.functions.async.RichAsyncFunction;
 import org.apache.flink.twitter.shaded.com.google.common.util.concurrent.FutureCallback;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.flink.streaming.api.functions.async.ResultFuture;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.util.EntityUtils;
+import org.asynchttpclient.AsyncHttpClient;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -16,23 +24,50 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.function.Supplier;
 // https://ci.apache.org/projects/flink/flink-docs-release-1.8/dev/stream/operators/asyncio.html Docs
-public class dockerAsync extends RichAsyncFunction<TweetData, String > {
+public class dockerAsync extends RichAsyncFunction<TweetData, TweetData > {
     private transient ExecutorService executorService;
 
     @Override
-    public void asyncInvoke(TweetData tweet, ResultFuture<String> resultFuture) throws java.io.IOException {
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        String jsonString ="{\"message\":\"Inoffensive and unremarkable.\" }";
+    public void asyncInvoke(TweetData tweet, ResultFuture<TweetData> resultFuture) throws java.io.IOException {
+        CloseableHttpAsyncClient httpClient = HttpAsyncClients.createDefault();
+        httpClient.start();
+        String jsonString ="{\"message\":\"" + tweet.tweetText + "\"}";
         HttpPost request = new HttpPost("http://localhost:5000/api/v1/analyzeSentiment");
-        StringEntity params = new StringEntity(jsonString);
-        request.addHeader("content-type", "application/x-www-form-urlencoded");
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        StringEntity params = new StringEntity(jsonString, ContentType.APPLICATION_FORM_URLENCODED);
+        //request.addHeader("content-type", "application/x-www-form-urlencoded");
         request.setEntity(params);
 
+        //issue the asynchronous request, receive a future for result
+        final Future<HttpResponse> result = httpClient.execute(request, null);
 
+        // set the callback to be executed once the request by the client is complete
+        // the callback simply forwards the result to the result future
+        CompletableFuture.supplyAsync(new Supplier<String>(){
+
+            @Override
+            public String get() {
+                try {
+
+                    String json = IOUtils.toString(result.get().getEntity().getContent());
+                    System.out.println(json);
+                    return result.get().getEntity().toString();
+                } catch (InterruptedException | ExecutionException | IOException e) {
+                    // Normally handled explicitly.
+                    return null;
+                }
+            }
+        }).thenAccept( (String sentResult) -> {
+            tweet.sentiment = sentResult;
+            resultFuture.complete(Collections.singleton(tweet));
+        });
+    }
 
     }
 
@@ -56,5 +91,5 @@ public class dockerAsync extends RichAsyncFunction<TweetData, String > {
 
 
 
-    }
+
 
--- src/main/java/twitterCEP.java
+++ src/main/java/twitterCEP.java
@@ -4,14 +4,25 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.Tumble;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.concurrent.TimeUnit;
+
 
 public class twitterCEP {
     public static void main(String args[]) throws IOException, Exception {
@@ -24,13 +35,25 @@ public class twitterCEP {
         TwitterSource twitterConnect = new TwitterSource(params.getProperties());
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<String> twitterStream = env.addSource(twitterConnect);
-        twitterStream.map(new BasicTweet()).filter(new FilterFunction<TweetData>() {
+        DataStream<Tuple3<String, String, Integer>> sentimentStream= twitterStream
+                .map(new BasicTweet()).filter(new FilterFunction<TweetData>() {
             @Override
             public boolean filter(TweetData tweetData) throws Exception {
-                return tweetData.language.equals("en");
+                return tweetData.language.equals("en") && tweetData.properHashtag;
             }
-        }).map(new flairMap()).flatMap(new TwitterTableMap());
+        }).flatMap(new TwitterTableMap());
+        //sentimentStream.keyBy(0).timeWindow(Time.seconds(10)).sum(2).print();
+        DataStream<TweetData> tweetStream =  twitterStream.map(new BasicTweet());
+        tweetStream = AsyncDataStream.unorderedWait( tweetStream, new dockerAsync(), 1000, TimeUnit.MILLISECONDS, 100);
+        tweetStream.print();
+       // sentimentStream.writeAsCsv("file.csv");
 
+        //StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+        //Table table = tableEnv.fromDataStream(sentimentStream, "rawText, sentiment, timeStamp");
+
+        //tableEnv.sqlQuery("SELECT * FROM ");
+        //tableEnv.registerDataStream("sentimentTable", sentimentStream, "rawText, sentiment, timeStamp");
+        //tableEnv.
         env.execute("Flair Filter");
     }
 }
