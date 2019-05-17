aa156f0422643137b5dd56e7b1e2e29e0d7dc623
Isaac Godfried <igodfried@isaac26.com>
2019-05-17 19:37:03 -0400

    Fix tweet tuple

--- src/main/java/tweetTupleMap.java
+++ src/main/java/tweetTupleMap.java
@@ -1,9 +1,10 @@
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 
-public class tweetTupleMap implements MapFunction<TweetData, Tuple4<String, String, String, Integer>> {
+public class tweetTupleMap implements MapFunction<TweetData, Tuple3<String, String, Integer>> {
     @Override
-    public Tuple4<String, String, String, Integer> map(TweetData tweetData) throws Exception {
-        return new Tuple4<>("nada", tweetData.sentiment, tweetData.tweetDateTime, 1);
+    public Tuple3<String, String, Integer> map(TweetData tweetData) throws Exception {
+        return new Tuple3<>(tweetData.tweetText, tweetData.tweetDateTime, 1);
     }
 }
